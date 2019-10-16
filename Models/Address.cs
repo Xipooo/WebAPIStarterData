858ffc734cd35e86ccd575acd39886f6bda85e66
Steve Bishop <xipooo@gmail.com>
2019-10-16 15:54:27 -0700

    Added InMemory Database

---
+++ Models/Address.cs
@@ -0,0 +1,13 @@
+namespace WebAPIStarterData.Models
+{
+    public class Address
+    {
+        public long Id { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string City { get; set; }
+        public string StateProvince { get; set; }
+        public string Zipcode { get; set; }
+        public string Country { get; set; }
+    }
+}
